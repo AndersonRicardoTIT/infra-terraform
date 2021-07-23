c106430db74a3f368dc898b5d65a05a819c76609
AndersonRicardoTIT <87536595+AndersonRicardoTIT@users.noreply.github.com>
2021-07-23 16:06:31 -0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  description = "Nome resource group"
+  type = string
+}
+
+variable "location" {
+  description = "localizacao do resource group"
+  type = string
+}
