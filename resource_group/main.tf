7134f1b2369259f287447c6cf801f2b5db83e8ce
AndersonRicardoTIT <anderson.costa@titcs.com.br>
2021-07-27 16:48:34 -0300

    update

--- resource_group/main.tf
+++ resource_group/main.tf
@@ -2,11 +2,3 @@ resource "azurerm_resource_group" "rg" {
   name = var.resource_group_name
   location = var.location
 }
-
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
-}
-
-output "location" {
-  value = azurerm_resource_group.rg.location
-}
