e91a6c0f4af128ac63f93ffdef264c6643194956
AndersonRicardoTIT <anderson.costa@titcs.com.br>
2021-07-27 16:32:54 -0300

    update

--- storage_acc/main.tf
+++ storage_acc/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "storageacc01" {
-  name                     = "storageacc01"
+  name                     = "storageaccount01${var.environment}"
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
